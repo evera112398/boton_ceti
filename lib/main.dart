61bcfb0b0d9d5bcd528970bc031fc505acb13dec
Edwin Vera~ <evera@c5jalisco.gob.mx>
2024-04-23 15:58:00 -0600

    Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:boton_ceti/controllers/alertas_controller.dart';
 import 'package:boton_ceti/views/home_screen.dart';
-import 'package:boton_ceti/views/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -42,15 +41,10 @@ class _BotonCetiState extends State<BotonCeti> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Botón CETI',
-<<<<<<< HEAD
       initialRoute: 'homeScreen',
       routes: {
         'homeScreen': (context) => const HomeScreen(),
       },
-=======
-      initialRoute: 'logincreen',
-      routes: {'logincreen': (context) => const Logincreen()},
->>>>>>> ff9fc06031c6d7c30e8265e61856fe14e0eb0003
     );
   }
 }
